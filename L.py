4862bf039110e0466ea8329b43bc78ceaa5536fa
zhangsaixian <2464919054@qq.com>
2024-12-25 21:36:04 +0800

    Update L.py

--- L.py
+++ L.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
 # 加载数据
 train_data = pd.read_csv('D:\\train_data.csv')
@@ -23,9 +24,17 @@ y_dev = dev_data['SoH']
 
 X_test = test_data[['cycle', 'capacity', 'resistance', 'CCCT', 'CVCT']]
 
+# 构建神经网络模型
+model = Sequential()
+model.add(Dense(64, input_dim=5, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1))
+
+# 编译模型
+model.compile(optimizer='adam', loss='mean_squared_error')
+
 # 训练模型
-model = RandomForestRegressor(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+model.fit(X_train, y_train, epochs=100, batch_size=10, validation_data=(X_dev, y_dev))
 
 # 在验证集上评估模型
 y_pred_dev = model.predict(X_dev)
@@ -39,7 +48,7 @@ y_pred_test = model.predict(X_test)
 submission = pd.DataFrame({
     'cycle': test_data['cycle'],
     'CS_Name': test_data['CS_Name'],
-    'result': y_pred_test
+    'result': y_pred_test.flatten()
 })
 
 # 保存为CSV文件
